62129c5316623346eb5e24315f76180f01db5c23
kusballer <KusBaller@gmail.com>
2025-10-12 22:23:19 -0400

    Made changes to greeting -kus

--- src/TeamCobra.java
+++ src/TeamCobra.java
@@ -3,7 +3,7 @@ public class TeamCobra
 {
 	public String greet() 
 	{
-
+		System.out.println("Hello Bye 121324");
 		return "Hello Team Cobra! It's nice working with you all!\n>>>>> Team members: Marcquesha, Hina, Kus, Melanie, and Ba";
 	}
 }
